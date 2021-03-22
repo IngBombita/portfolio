26ab65e7185275bf5bcafbbcbf2ae30b308297ad
BomberMan <eduardojulioculasso@gmail.com>
2021-03-22 03:24:53 -0300

    expose env variable

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    fetch(process.env.WEBHOOK_URL, {
+    fetch("https://hooks.zapier.com/hooks/catch/5646471/o7llsnz/", {
         method: "POST",
         body: JSON.stringify(req.body)
     });
